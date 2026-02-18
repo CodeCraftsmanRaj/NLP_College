29c8b7a4b675cb3ab4e6ce759510edf11ee24671
raj_99 <rajmathuria79@gmail.com>
2026-02-18 10:28:00 +0530

    Fix: prints

--- Exp4/exp4.py
+++ Exp4/exp4.py
@@ -8,31 +8,38 @@ from hmmlearn import hmm
 nltk.download('brown')
 nltk.download('universal_tagset')
 
-tagged_sentences = list(brown.tagged_sents(tagset='universal'))[:5000]
+# Print full matrices without truncation
+np.set_printoptions(threshold=np.inf, linewidth=200)
+
+# Take ONLY ONE sentence from Brown corpus
+tagged_sentence = brown.tagged_sents(tagset='universal')[100]
+
+print("\nSentence Used For Computation:\n")
+print([word.lower() for word, tag in tagged_sentence])
+print("\nTagged Version:\n")
+print(tagged_sentence)
 
 transition_counts = defaultdict(Counter)
 emission_counts = defaultdict(Counter)
 tag_counts = Counter()
 start_counts = Counter()
 
-for sentence in tagged_sentences:
-    if len(sentence) == 0:
-        continue
+if len(tagged_sentence) != 0:
 
-    first_tag = sentence[0][1]
+    first_tag = tagged_sentence[0][1]
     start_counts[first_tag] += 1
 
-    for i, (word, tag) in enumerate(sentence):
+    for i, (word, tag) in enumerate(tagged_sentence):
         word = word.lower()
         emission_counts[tag][word] += 1
         tag_counts[tag] += 1
 
         if i > 0:
-            prev_tag = sentence[i - 1][1]
+            prev_tag = tagged_sentence[i - 1][1]
             transition_counts[prev_tag][tag] += 1
 
 tags = sorted(tag_counts.keys())
-vocab = sorted({word.lower() for sentence in tagged_sentences for word, _ in sentence})
+vocab = sorted({word.lower() for word, _ in tagged_sentence})
 
 tag_index = {tag: i for i, tag in enumerate(tags)}
 word_index = {word: i for i, word in enumerate(vocab)}
@@ -44,33 +51,51 @@ start_prob = np.zeros(N)
 transition_matrix = np.zeros((N, N))
 emission_matrix = np.zeros((N, V))
 
+# Start probability
 for tag in tags:
-    start_prob[tag_index[tag]] = start_counts[tag] / sum(start_counts.values())
+    start_prob[tag_index[tag]] = start_counts[tag]
 
+start_prob = start_prob / start_prob.sum()
+
+# Transition matrix
 for prev_tag in tags:
     for tag in tags:
         transition_matrix[tag_index[prev_tag]][tag_index[tag]] = transition_counts[prev_tag][tag]
 
-transition_matrix = transition_matrix / transition_matrix.sum(axis=1, keepdims=True)
+if transition_matrix.sum(axis=1).any():
+    transition_matrix = np.divide(
+        transition_matrix,
+        transition_matrix.sum(axis=1, keepdims=True),
+        where=transition_matrix.sum(axis=1, keepdims=True) != 0
+    )
 
+# Emission matrix
 for tag in tags:
     for word in vocab:
         emission_matrix[tag_index[tag]][word_index[word]] = emission_counts[tag][word]
 
-emission_matrix = emission_matrix / emission_matrix.sum(axis=1, keepdims=True)
+if emission_matrix.sum(axis=1).any():
+    emission_matrix = np.divide(
+        emission_matrix,
+        emission_matrix.sum(axis=1, keepdims=True),
+        where=emission_matrix.sum(axis=1, keepdims=True) != 0
+    )
 
-print("Transition Matrix:\n")
+print("\nFull Transition Matrix:\n")
 print(transition_matrix)
 
-print("\nEmission Matrix:\n")
+print("\nFull Emission Matrix:\n")
 print(emission_matrix)
 
+# Sentence words only
+sentence = [word.lower() for word, tag in tagged_sentence]
+
 def get_possible_tags(word):
-    word = word.lower()
     return [tag for tag in tags if emission_counts[tag][word] > 0]
 
 def manual_probability(sentence, tag_sequence):
-    prob = start_prob[tag_index[tag_sequence[0]]] * emission_matrix[tag_index[tag_sequence[0]]][word_index[sentence[0]]]
+    prob = start_prob[tag_index[tag_sequence[0]]] * \
+           emission_matrix[tag_index[tag_sequence[0]]][word_index[sentence[0]]]
 
     for i in range(1, len(sentence)):
         prob *= transition_matrix[tag_index[tag_sequence[i - 1]]][tag_index[tag_sequence[i]]]
@@ -78,16 +103,14 @@ def manual_probability(sentence, tag_sequence):
 
     return prob
 
-test_sentence = ["they", "park", "cars"]
-
-possible_tags_per_word = [get_possible_tags(word) for word in test_sentence]
+possible_tags_per_word = [get_possible_tags(word) for word in sentence]
 all_paths = list(product(*possible_tags_per_word))
 
 best_path = None
 best_prob = 0
 
 for path in all_paths:
-    prob = manual_probability(test_sentence, path)
+    prob = manual_probability(sentence, path)
     if prob > best_prob:
         best_prob = prob
         best_path = path
@@ -97,7 +120,7 @@ model.startprob_ = start_prob
 model.transmat_ = transition_matrix
 model.emissionprob_ = emission_matrix
 
-obs = np.array([[word_index[w.lower()]] for w in test_sentence])
+obs = np.array([[word_index[w]] for w in sentence])
 
 log_prob_score = model.score(obs)
 library_prob = np.exp(log_prob_score)
